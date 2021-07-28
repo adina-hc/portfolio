327edbf88c8530acad729677f67c8b7e36f56411
Adina Henderson <adinahenderson.mail@gmail.com>
2021-07-28 17:34:59 -0400

    updated code to initialize app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PortfolioBox from './components/PortfolioBox';
+import Header from './components/PortfolioBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//import logo from './logo.svg';
 
+//import './App.css';
+
+const App = () => <PortfolioBox />
+  
 export default App;
