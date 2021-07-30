86e2c5dbfc60df52adcd6e8b4d8526208e43378a
Adina Henderson <adinahenderson.mail@gmail.com>
2021-07-30 18:26:40 -0400

    Modified code to make a one pager portfolio

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,19 @@
+// Import resources
+import React from 'react';
+import About from '../components/About';
+import Work from '../components/Work';
+import Resume from '../components/Resume';
+import Contact from '../components/Contact';
+
+const Portfolio = () => {
+  return (
+    <>
+      <About />
+      <Work />
+      <Resume />
+      <Contact />
+    </>
+  )
+}
+
+export default Portfolio;
