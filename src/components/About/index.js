387dc5eff88a990e2375c63720bb6927c7760b84
Adina Henderson <adinahenderson.mail@gmail.com>
2021-07-30 19:03:30 -0400

    translated about section into react

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <section id="about-me" class="about-me">
+            <div class="subtitle-about-me subtitles">
+                <h2>About Me</h2>
+            </div>        
+            <article class="article-content"> 
+                <p> Full Stack Web Developer in the making.
+                    Currently a student of Tecnologico de Monterrey's Full Stack Web Development Bootcamp.
+                    Coming from a Business, Accounting & Auditing, Finance and Consulting background, I am interested and preparing for a career change.
+                    I love challenge, languages, music and dance.
+                </p>
+            </article>
+            </section>
+        </div>
+    )
+}
+
+export default About;
