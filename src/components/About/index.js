cd0a23c514f22a2dab465428b364bb4a866c34d6
Adina Henderson <adinahenderson.mail@gmail.com>
2021-10-28 17:46:10 -0400

    adjusted About section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import profilePic from "../../images/adina-portfolio.JPG";
-import backGPic from '../../images/matrix.jpg';
-import Footer from '../Footer/index';
+import bgPic from "../../images/hero.png"
+import Footer from "../Footer"
+
 
 // Import style
 import '../../reset.css'
@@ -11,38 +12,42 @@ import '../../fix-materialize.css'
 function About() {
     return (
       <>
-        <div style={{ backgroundImage: `url(${backGPic})` }}>
-          <section id="about-me" class="about-me">
-            <div className=" subtitle">
-              <h2 className="subtitle-about-me subtitles"> </h2>
-            </div>
-            <div className="center-div">
-              <h1 className="developer-name">Adina Henderson</h1>
-              <img className="profile-picture" src={profilePic} alt=""></img>
-
-              <article class="article-content">
+        <div
+          className="bgImage"
+          style={{ backgroundImage: `url(${bgPic})` }}
+        ></div>
+        <section id="about-me" className="about-me">
+          <div className=" subtitle">
+            <h2 className="subtitle-about-me subtitles"> </h2>
+          </div>
+          <div className="center-div">
+            <h1 className="developer-name">Adina Henderson</h1>
+            <img className="profile-picture" src={profilePic} alt=""></img>
+            <div className="article-container">
+              <article className="article-content">
                 <p>
-                  Full stack web developer educated at Tec de Monterrey. Well
-                  versed with HTML, CSS, JavaScript, among other technologies.
-                  Proven staff management, as well as project management and
-                  development abilities, excellent interpersonal and
-                  communication skills. Passionate about database design. Highly
-                  successful in collaborative environments, while independently
-                  working, adaptable to change, problem solver, highly
-                  motivated, organized, and passionate about software design and
-                  development, data analysis, quality assurance, etc. Fluent in
-                  English and Spanish. Recently worked in diverse projects
-                  including different technologies: MERN, React, Databases (mySQL
-                  and NoSQL), Mongo, node.js, JavaScript, among others.
+                  Educated at Tecnologico de Monterrey
+                  <br />
+                  Worked with HTML, CSS, JavaScript, among other technologies.{" "}
+                  <br />
+                  <br />
+                  Passionate about database and software design, data analysis,
+                  quality assurance, etc.
+                  <br />
+                  Recently worked in diverse projects including different
+                  technologies: MERN, React, Databases (SQL and NoSQL), node.js,
+                  JavaScript, among others.
+                  <br />
                   <br />
                   Coming from a Business, Accounting & Auditing, Finance and
                   Consulting background, I am eager to leap into the Technology
-                  field. I love challenge, languages, music and dance.
+                  field. I love challenge, learning, languages, music, and
+                  dance.
                 </p>
               </article>
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
         <Footer />
       </>
     );
