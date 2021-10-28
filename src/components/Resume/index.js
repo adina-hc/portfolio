2cba096e06982efdaa6304bcfa76fc1af3d31ad1
Adina Henderson <adinahenderson.mail@gmail.com>
2021-10-28 18:43:08 -0400

    replaced pdf document and adjusted layout

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,22 +1,31 @@
 // Import resources
 import React from 'react'
 import resumeDoc from '../../images/AH-Resume.pdf'
-import Footer from '../Footer/index';
+import Footer from '../Footer';
+
 
 const Resume = () => {
     return (
-        <div>
-            <section id="resume"  class="resume">
-            <div class="subtitle-resume subtitles">
-                <h2>Resume</h2>
-            </div>
-            <article class="article-content">
-                <iframe title="Display Resume" type="application/pdf" src={resumeDoc} style={{height: 620}} frameborder="0"></iframe>
+      <div>
+        <section id="resume" className="resume">
+          <div className="subtitle-resume subtitles">
+            <h2>Resume</h2>
+          </div>
+          <div className="article-content">
+            <article className="article-content-resume">
+              <iframe
+                title="Display Resume"
+                type="application/pdf"
+                src={resumeDoc}
+                style={{ height: 620 }}
+                frameBorder="0"
+              ></iframe>
             </article>
+          </div>
         </section>
         <Footer />
-        </div>
-    )
+      </div>
+    );
 }
 
 export default Resume;
