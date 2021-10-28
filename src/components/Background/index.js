7ffa3f2d0565523f4a756e56a8f51bb1a9a052cc
Adina Henderson <adinahenderson.mail@gmail.com>
2021-10-28 18:45:02 -0400

    I may remove this component if not needed

---
+++ src/components/Background/index.js
@@ -0,0 +1,15 @@
+import backGPic from "../../images/hero.png";
+
+
+
+import React from 'react'
+
+const Background = () => {
+    return (
+        <div>           
+            <img src={backGPic} alt="Background Pic" className="" />
+        </div>
+    )
+}
+
+export default Background
